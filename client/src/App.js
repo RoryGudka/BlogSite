947d3658b44826752477a1ca687e5173bdecc116
RoryGudka <rdgudka@gmail.com>
2021-06-07 11:49:10 -0400

    Added routing and user context

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.css";
+import Login from "./components/Login";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import UserContextProvider from "./contexts/UserContextProvider";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <UserContextProvider>
+        <Switch>
+          <Route path="/" exact component={<div></div>}></Route>
+          <Route path="/login" component={Login}></Route>
+        </Switch>
+      </UserContextProvider>
+    </Router>
   );
 }
 
