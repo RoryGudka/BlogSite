4caabc94fd83cad612cc8169973ceef72ae727ae
sid_tickle <st3ehn@virginia.edu>
2021-06-08 16:59:08 -0400

    updated name for NavBar component to prevent error

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Landing from "./components/Landing";
 import MerchandisePage from "./components/MerchandisePage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import UserContextProvider from "./contexts/UserContextProvider";
-import Navbar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 import Signup from "./components/Signup";
 import Footer from "./components/Footer";
 
@@ -13,12 +13,12 @@ function App() {
     <Router>
       <UserContextProvider>
         <div id="bodyColor"></div>
-        <Navbar />
+        <NavBar />
         <Switch>
           <Route path="/" exact component={Landing}></Route>
           <Route path="/login" component={Login}></Route>
           <Route path="/signup" component={Signup}></Route>
-          <Route path="/store" component={MerchandisePage}></Route>
+          <Route path="/shop" component={MerchandisePage}></Route>
         </Switch>
         <Footer />
       </UserContextProvider>
