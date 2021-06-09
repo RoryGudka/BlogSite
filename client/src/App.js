f3371d8ac2745e922b1cf19638893f1e706eef5a
CooperDeVito <ced3rq@virginia.edu>
2021-06-09 16:27:36 -0400

    moved merchinfo into components

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,35 @@
-import "./styles/App.css";
-import Login from "./components/Login";
-import Landing from "./components/Landing";
-import MerchandisePage from "./components/MerchandisePage";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import UserContextProvider from "./contexts/UserContextProvider";
+import './styles/App.css';
+import Login from './components/Login';
+import Landing from './components/Landing';
+import MerchandisePage from './components/MerchandisePage';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import UserContextProvider from './contexts/UserContextProvider';
 import Paper from '@material-ui/core/Paper';
-import MerchInfo from './MerchInfo/MerchInfo';
-import NavBar from "./components/Navbar";
-import Signup from "./components/Signup";
-import Footer from "./components/Footer";
-import {BlogDash, ForumDash} from './components/PostComponents/PostDashes';
+import MerchInfo from './components/MerchInfo/MerchInfo';
+import NavBar from './components/Navbar';
+import Signup from './components/Signup';
+import Footer from './components/Footer';
+import { BlogDash, ForumDash } from './components/PostComponents/PostDashes';
 
 function App() {
-  return (
-    <Router>
-      <UserContextProvider>
-        <div id="bodyColor"></div>
-        <NavBar />
-        <Switch>
-          <Route path="/" exact component={Landing}></Route>
-          <Route path="/login" component={Login}></Route>
-          <Route path="/item" component={MerchInfo}></Route>
-          <Route path="/signup" component={Signup}></Route>
-          <Route path="/shop" component={MerchandisePage}></Route>
-          <Route path="/blog" component={BlogDash}></Route>
-          <Route path="/forum" component={ForumDash}></Route>
-        </Switch>
-        <Footer />
-      </UserContextProvider>
-    </Router>
-  );
+	return (
+		<Router>
+			<UserContextProvider>
+				<div id="bodyColor"></div>
+				<NavBar />
+				<Switch>
+					<Route path="/" exact component={Landing}></Route>
+					<Route path="/login" component={Login}></Route>
+					<Route path="/item" component={MerchInfo}></Route>
+					<Route path="/signup" component={Signup}></Route>
+					<Route path="/shop" component={MerchandisePage}></Route>
+					<Route path="/blog" component={BlogDash}></Route>
+					<Route path="/forum" component={ForumDash}></Route>
+				</Switch>
+				<Footer />
+			</UserContextProvider>
+		</Router>
+	);
 }
 
 export default App;
